fd8e916299e1bf8f14fd147e54beba62fa55865a
Antonio <47687247+tonyhristov@users.noreply.github.com>
2021-02-08 19:03:05 +0200

    feat: half of the navigation is done

---
+++ src/components/header/index.js
@@ -0,0 +1,14 @@
+import { React } from 'react'
+import styles from './index.module.css'
+
+const Header = () => {
+   return (
+      <header>
+         <h1 className={styles.t}>
+            Test nfsfkld s jfndgnsflgn sdkjn nfdlgnsdkljngfnjgdskSss
+         </h1>
+      </header>
+   )
+}
+
+export default Header
