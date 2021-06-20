405b5c8f3f14b052415d40dec417537e3ff23bdb
Antonio Hristov <tonyhristow@gmail.com>
2021-06-21 00:05:03 +0300

    fix: aside

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import PageLayout from '../../components/page-layout'
 import styles from './index.module.css'
+import computer from '../../images/computer (1).svg'
 
 const HomePage = () => {
    return (
       <PageLayout>
-         <div className={styles.background}></div>
+         <div className={styles.background}>
+            {/* <img src={computer} alt='React Logo' className={styles.image} /> */}
+         </div>
       </PageLayout>
    )
 }
