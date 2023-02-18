5b7471c794575298af30339c99644f0d08a1d5db
Antonio Hristov <tonyhristow@gmail.com>
2023-02-18 18:21:14 +0200

    feat: add logo for navbar

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import getLinks from '../../utils/navigation';
 import styled from 'styled-components';
 import NavBarLink from '../navLink';
+import logo from '../../images/Logo-transparent.png';
+import hue from '../../constants/hue';
 
 const Header = () => {
     const links = getLinks;
 
     return (
         <Nav>
+            <GradientLogoFiller key='background-logo'>
+                <Logo src={logo} />;
+            </GradientLogoFiller>
             {links.map((link) => (
                 <NavBarLink props={link} key={link.title} />
             ))}
@@ -22,6 +27,22 @@ const Nav = styled.nav`
     align-items: center;
     overflow: hidden;
     background-color: #000000;
+`;
+
+const GradientLogoFiller = styled.div`
+    background-color: #f35626;
+    background-image: -webkit-linear-gradient(92deg, #f35626, #feab3a);
+    -webkit-animation: ${hue} 10s infinite linear;
+    width: 20vh;
+    height: 5vh;
+    position: relative;
+    overflow: hidden;
+`;
+
+const Logo = styled.img`
+    width: 20vh;
+    height: 5vh;
+    object-fit: fill;
 `;
 
 export default Header;
