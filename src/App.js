f4c655cc48c990ccb0242ff44aa992aa0d3c1e12
Antonio Hristov <tonyhristow@gmail.com>
2023-02-18 15:08:08 +0200

    feat: add in development screen

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import { React } from 'react'
-import './App.css'
-import styled from "styled-components";
+import { React } from 'react';
+import './App.css';
+import styled from 'styled-components';
+import hue from './constants/hue';
 
-const App = (props) => (
-   <AppContainer>
-      {props.children}
-   </AppContainer>
-)
+function App() {
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                <CompanyText>Antonio Hristov | Personal Portfolio</CompanyText>
+                <AnimatedGradientText>In Development</AnimatedGradientText>
+                <BlueText tagName={'h3'}>Stay tuned :)</BlueText>
+            </header>
+        </div>
+    );
+}
 
-const AppContainer = styled.div`
-   background-color: #171717;
-`
+const AnimatedGradientText = styled.h2`
+    color: #f35626;
+    background-image: -webkit-linear-gradient(92deg, #f35626, #feab3a);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    -webkit-animation: ${hue} 10s infinite linear;
+    font-size: 7vh;
+    text-align: center;
+`;
+const CompanyText = styled.h1`
+    color: #02c6ff;
+`;
+const BlueText = styled.h3`
+    color: #02c6ff;
+`;
 
-
-export default App
+export default App;
