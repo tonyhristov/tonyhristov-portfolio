11e5fbd93330aa998ec4fb6dd57566b5492560c4
Antonio Hristov <tonyhristow@gmail.com>
2023-10-20 16:43:30 +0300

    feat: navbar is finished

--- src/App.js
+++ src/App.js
@@ -1,34 +1,11 @@
 import { React } from 'react';
 import './App.css';
 import styled from 'styled-components';
-import hue from './constants/hue';
 
-function App() {
-    return (
-        <div className='App'>
-            <header className='App-header'>
-                <CompanyText>Antonio Hristov | Personal Portfolio</CompanyText>
-                <AnimatedGradientText>In Development</AnimatedGradientText>
-                <BlueText tagName={'h3'}>Stay tuned :)</BlueText>
-            </header>
-        </div>
-    );
-}
+const App = (props) => <AppContainer>{props.children}</AppContainer>;
 
-const AnimatedGradientText = styled.h2`
-    color: #f35626;
-    background-image: -webkit-linear-gradient(92deg, #f35626, #feab3a);
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
-    -webkit-animation: ${hue} 10s infinite linear;
-    font-size: 7vh;
-    text-align: center;
-`;
-const CompanyText = styled.h1`
-    color: #02c6ff;
-`;
-const BlueText = styled.h3`
-    color: #02c6ff;
+const AppContainer = styled.div`
+    background-color: #171717;
 `;
 
 export default App;
