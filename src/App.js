eb4c7f64442ab2703d50855e737d3ac09183d6bb
Antonio Hristov <tonyhristow@gmail.com>
2023-12-24 18:44:47 +0200

    feat: add vercel SpeedInsights (#56)
    * feat: add vercel SpeedInsights
    
    * fix: remove console.log

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import { React } from 'react';
 import './App.css';
 import styled from 'styled-components';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
-const App = (props) => <AppContainer>{props.children}</AppContainer>;
+const App = (props) => (
+    <AppContainer>
+        {props.children}
+        <SpeedInsights />
+    </AppContainer>
+);
 
 const AppContainer = styled.div`
     background-color: #171717;
